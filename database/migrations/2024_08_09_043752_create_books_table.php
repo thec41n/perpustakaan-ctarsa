f885134bb4989ac2e2c533db183bc86bfb416e79
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-08-09 11:39:07 +0700

    feat: database migration

---
+++ database/migrations/2024_08_09_043752_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('category_id')->constrained();
+            $table->string('author'); 
+            $table->string('publisher');
+            $table->year('year');
+            $table->text('description')->nullable(); 
+            $table->integer('quantity');
+            $table->string('file_path');
+            $table->string('cover_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
